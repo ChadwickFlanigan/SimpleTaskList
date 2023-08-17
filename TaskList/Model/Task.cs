8f5e0ffe83f0743432ac934434d17f9b906c294f
ChadwickFlanigan <132013220+ChadwickFlanigan@users.noreply.github.com>
2023-08-17 14:07:33 -0400

    gave Task model properties

--- TaskList/Model/Task.cs
+++ TaskList/Model/Task.cs
@@ -8,5 +8,9 @@ namespace TaskList.Model
 {
     internal class Task
     {
+        private int TaskID (Get; Set;)
+        private string TaskName (Get; Set;)
+        private string TaskDescription (Get; Set;)
+        private bool Complete (Get; Set;)
     }
 }
