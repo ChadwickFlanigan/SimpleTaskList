92fb0bf6a68280f3e9021d3520aea7833827ed26
Chad Flanigan <chazzflanigan@gmail.com>
2023-08-23 11:53:27 -0400

    created TaskListConnectionString class to create the SQL connection string

---
+++ TaskList/DALs/TaskListConnectionString.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Microsoft.Data.SqlClient;
+
+namespace TaskList.DALs
+{
+    /// <summary>
+    /// class creating the connection string to the database
+    /// </summary>
+    public static class TaskListConnectionString
+    {
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection("Data Source=localhost;Initial Catalog=SimpleTaskList;Integrated Security=True;");
+        }
+    }
+}
